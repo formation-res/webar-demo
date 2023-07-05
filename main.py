86f81fc7be8e84443ce2b403419712a94235cdbc
Athena Rapinan <arapinan@uci.edu>
2023-07-05 10:36:24 +0200

    wrote the filter() function; filters by title

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# how to parse the data in the spreadsheet.
+# parses the data in the spreadsheet.
 # yields the filtered corrdinates that meet the criteria, and stores them into a list.
 
 import csv
 
 
-def filter():
+def filter(title, current_title: str):
     """
     Decide if the row being examined meets the criteria.
     If the row meets the criteria (the filter), then True will be returned and the row will be stored in points_collection.
     """
-    return True
+    if title is None:
+        return True
+    else:
+        if title == current_title:
+            return True
+        else:
+            return False
 
 
-def parse_coordinates():
+def parse_coordinates(title=None):
     """
     A generator function to parse through each line of the csv file (the google spreadsheet with point/object information).
     The (id, title, long, lat, color, category, shape) is yielded for each row that meets the filter criteria.
@@ -20,7 +26,7 @@ def parse_coordinates():
     with open('points.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            if filter():
+            if filter(title, row[5]):
                 x = row[7]
                 first = x.find('[')
                 second = x.find(']')
@@ -29,11 +35,11 @@ def parse_coordinates():
                 new = draft.split('_COMMA_')
                 long = new[0]
                 lat = new[-1]
-                
+
                 yield (row[0], row[5], long, lat, row[10], row[17], row[25])
 
 
-csv_line_generator_filtered = parse_coordinates()
+csv_line_generator_filtered = parse_coordinates('Toilet')
 points_collection = [i for i in csv_line_generator_filtered]
 for i in points_collection:
     print(i)
