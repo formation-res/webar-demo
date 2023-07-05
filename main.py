280be4c2f8da6d1ef339a9d0be0062001de6fd7a
Athena Rapinan <arapinan@uci.edu>
2023-07-05 14:08:56 +0200

    finished getting angle

--- main.py
+++ main.py
@@ -41,13 +41,15 @@ def parse_coordinates(title=None):
                 yield (row[0], row[5], long, lat, row[10], row[17], row[25])
 
 
+# generate a list of points we want to display with web ar:
 csv_line_generator_filtered = parse_coordinates('Toilet')
 points_collection = [i for i in csv_line_generator_filtered]
 # for i in points_collection:
 #      print(i)
 #      print()
 
-    
+
+# find the distance between two points:
 lat1 = float(points_collection[1][2])
 long1 = float(points_collection[1][3])
 p1 = (lat1, long1)
@@ -56,4 +58,17 @@ lat2 = float(points_collection[0][2])
 long2 = float(points_collection[0][3])
 p2 = (lat2, long2)
 
-print("distance between two points:", vincenty(p1,p2)*1000, " meters.")
+print("distance between the first two points:", vincenty(p1,p2)*1000, " meters.")
+
+
+# find the angle between two points:
+def find_angle(p1, p2):
+    distance_x = p2[0] - p1[0]
+    distance_y = p2[1] - p1[1]
+
+    angle_radians = np.arctan2(distance_y, distance_x)
+    angle_degrees = np.degrees(angle_radians)
+    fixed_angle_degrees = (angle_degrees + 360) % 360
+    return fixed_angle_degrees
+
+print(f'angle between the first two points: {find_angle(p1, p2)} degrees.')
