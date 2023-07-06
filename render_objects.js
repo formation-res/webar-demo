b13a1bee115386f1d99ff90ba589398ecf9c9895
Athena Rapinan <arapinan@uci.edu>
2023-07-06 15:05:48 +0200

    added filters, and now points pop up in web ar

--- render_objects.js
+++ render_objects.js
@@ -1,47 +1,56 @@
 import { ARButton } from 'https://unpkg.com/three@0.126.0/examples/jsm/webxr/ARButton.js';
-    
-		let camera, scene, renderer;
-    let mesh;
 
-		init();
-		animate();
+// added:
+var points_collection = JSON.parse(json_str)
+console.log(points_collection.length)
+//
 
-		function init() {
-			const container = document.createElement('div');
-			document.body.appendChild(container);
+let camera, scene, renderer;
+let mesh;
 
-			scene = new THREE.Scene();
+init();
+animate();
 
-			camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40);
+function init() {
+	const container = document.createElement('div');
+	document.body.appendChild(container);
 
-			renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setSize(window.innerWidth, window.innerHeight);
+	scene = new THREE.Scene();
 
-			renderer.xr.enabled = true; // New!
-			container.appendChild(renderer.domElement);
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40);
 
-			var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-			light.position.set(0.5, 1, 0.25);
-			scene.add(light);
-      
-      const geometry = new THREE.IcosahedronGeometry(0.1, 1);
-      const material = new THREE.MeshPhongMaterial({
-        color      :  new THREE.Color("rgb(226,35,213)"),
-        shininess  :  6,
+	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	renderer.xr.enabled = true;
+	container.appendChild(renderer.domElement);
+
+	var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+	light.position.set(0.5, 1, 0.25);
+	scene.add(light);
+
+    const geometry = new THREE.IcosahedronGeometry(0.1, 1);
+    const material = new THREE.MeshPhongMaterial({
+    	color      :  new THREE.Color("rgb(226,35,213)"),
+    	shininess  :  6,
         flatShading:  true,
         transparent: 1,
         opacity    : 0.8
       });
-      
-      mesh = new THREE.Mesh(geometry, material);
-      mesh.position.set(0, 0, -0.5);
-      scene.add(mesh);
-      
+    
+	// added:
+	for (var i = 0; i <points_collection.length ;i++) {
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.set((points_collection[i].x * 1), 0, (points_collection[i].y * 1));
+		scene.add(mesh);
 
-			document.body.appendChild(ARButton.createButton(renderer));
+		console.log(points_collection[i].x * 1)
+	}
+    //
 
-			window.addEventListener('resize', onWindowResize, false);
+		document.body.appendChild(ARButton.createButton(renderer));
+		window.addEventListener('resize', onWindowResize, false);
 		}
 
 		function onWindowResize() {
