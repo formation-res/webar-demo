a67361dd023b26313db304f8bbd43de854ce889a
Donovan Rimer <donnyjrimer@gmail.com>
2023-07-19 14:43:50 +0200

    orientation works

--- render_objects.js
+++ render_objects.js
@@ -257,14 +257,14 @@ function createPoints(angle) {
     var cos = Math.cos(radians);
     var sin = Math.sin(radians);
     var newX = (cos * x) + (sin * y);
-    var newY = (cos * y) - (sin * x);
+    var newY = - ((cos * y) - (sin * x)); //must be negative because -z = +y north
 
     //create the points in the scene. 
     /* 
     we want +z to correspond to +y axis of 2D
     */
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set( x, 0, -y);
+    mesh.position.set( newX, 0, newY);
     scene.add(mesh)
     
     }
