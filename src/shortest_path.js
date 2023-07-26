0ce3dad5aee04b8fb644a3ae145f9fa8eb294082
Donovan Rimer <donnyjrimer@gmail.com>
2023-07-26 16:16:47 +0200

    is any of it working on mobile?

--- src/shortest_path.js
+++ src/shortest_path.js
@@ -3,7 +3,7 @@ import { json_str } from "./data/icon_data.js";
 import { waypoints_raw } from "./data/waypoints_dump.js";
 import LatLon from 'geodesy/latlon-spherical.js';
 
-
+alert("here lies issue");
 const hits = waypoints_raw.data.search.hits;
 var points_collection = JSON.parse(json_str);
 const g = new WeightedGraph();
