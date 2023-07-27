2f24d9f13b6713c3d7dfaf90cd38780ead912c36
Donovan Rimer <donnyjrimer@gmail.com>
2023-07-27 16:10:52 +0200

    DOING THE EXACT SAME THING AS CREATE POOINTSgit add .git add .

--- src/render_objects.js
+++ src/render_objects.js
@@ -282,18 +282,21 @@ function createWayPoints(angle){
 		});
 	
 		//adjusted for the angle
-		var dis = waypoint_collection[element].distance;
-		var ang = waypoint_collection[element].angle;
+		// var dis = waypoint_collection[element].distance;
+		// var ang = waypoint_collection[element].angle;
 
-		var radians = (Math.PI / 180) * (angle+ang) ;
-		var cos = Math.cos(radians);
-		var sin = Math.sin(radians);
-		var newX = (cos * x) - (sin * y);
-		var newY = (cos * y) + (sin * x); //must be negative because -z = +y north
-	
-		mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set( newY, 0, newX )();
-		scene.add(mesh)
+		var x = waypoint_collection[element].x;
+		var y = waypoint_collection[element].y;
+
+		 var radians = (Math.PI / 180) * (angle);
+	     var cos = Math.cos(radians);
+   		 var sin = Math.sin(radians);
+   		 var newX = (cos * x) - (sin * y);
+   		 var newY = (cos * y) + (sin * x); //must be negative because -z = +y north
+
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set( newX, 0, -newY);
+    scene.add(mesh)
 		}
 }
 
