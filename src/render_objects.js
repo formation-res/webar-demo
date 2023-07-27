a1f191612eb457b4abcbe8357aad03077916b72c
Donovan Rimer <donnyjrimer@gmail.com>
2023-07-27 11:10:58 +0200

    trying chat GPT's method

--- src/render_objects.js
+++ src/render_objects.js
@@ -294,6 +294,10 @@ function createWayPoints(angle){
 		//adjusted for the angle
 		var x = waypoint_collection[element].x;
 		var y = waypoint_collection[element].y;
+
+		newP = rotatePoint(x,y,angle);
+
+
 		var radians = (Math.PI / 180) * angle;
 		var cos = Math.cos(radians);
 		var sin = Math.sin(radians);
@@ -301,7 +305,7 @@ function createWayPoints(angle){
 		var newY = (cos * y) + (sin * x); //must be negative because -z = +y north
 	
 		mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set( newX, 0, -newY);
+		mesh.position.set( newP.x , 0, -newP.y);
 		scene.add(mesh)
 		}
 }
