3bce3b7bc475a03076056c555e4abe6aa86ff11f
Donovan Rimer <donnyjrimer@gmail.com>
2023-07-27 11:07:42 +0200

    reverting

--- src/render_objects.js
+++ src/render_objects.js
@@ -296,14 +296,13 @@ function createWayPoints(angle){
 		var y = waypoint_collection[element].y;
 		var radians = (Math.PI / 180) * angle;
 		var cos = Math.cos(radians);
-    	var sin = Math.sin(radians);
-   		var newX = (cos * x) - (sin * y);
-   		var newY = (cos * y) + (sin * x); //must be negative because -z = +y north
-
-   		mesh = new THREE.Mesh(geometry, material);
-   		mesh.position.set( +newX, 0, newY);
-   		scene.add(mesh)
-		
+		var sin = Math.sin(radians);
+		var newX = (cos * x) - (sin * y);
+		var newY = (cos * y) + (sin * x); //must be negative because -z = +y north
+	
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.set( newX, 0, -newY);
+		scene.add(mesh)
 		}
 }
 
