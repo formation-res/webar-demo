6d448fdfc8187e9857ef36d3afe4cbdb3deb81b2
Athena Rapinan <arapinan@uci.edu>
2023-08-09 16:27:15 +0200

    final version of the search tool

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # webar-demo
+# Navigate between points in Web AR based on search criteria
 
-# Display icons in AR based on search criteria or radius away.
-
-nearly works when facing west.
+the module "./src/data/connect.py" connects to the FORMATION search API, and retrieves the results for a search query. I was unable to incorporate this with the current code. I kept it in this project for reference, just in case of future use.
 
 ### npm run dev -- --host 
 for network hosting.
--- src/data/connect.py
+++ src/data/connect.py
@@ -46,54 +46,6 @@ def CreateJson(obj: list, dest: str):
     print("export var json_str = '{}' ".format(json_obj) )
 
 
-import requests
-import sys, json
-
-
-def search(groupId: str, token:str, item: str) -> list:
-    url = 'https://api.tryformation.com/search'
-    headers = {'accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
-    payload = {"groupIds": [groupId]}
-
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['hits']
-    else:
-        print(f"Request failed with status code {response.status_code}")
-        print(response.text)
-
-
-def connect(item: str):
-    url = 'https://api.tryformation.com/token/loginToWorkspace'
-    headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
-    payload = {
-        "workspace": "demo",
-        "email": "athena@tryformation.com",
-        "password": "Athrap@11"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        token = data['apiAccessToken']['token']
-        groupId = data['groups'][0]['groupId']
-        groupName = data['groups'][0]['groupName']
-        userId = data['groups'][0]['userId']
-        return search(groupId, token, item)
-    else:
-        print(f"Request failed with status code {response.status_code}")
-
-
-def CreateJson(obj: list, dest: str):
-    outFile = "./src/data/" + dest  #dump the info into an out file
-    sys.stdout = open(outFile, 'w')
-
-    json_obj = json.dumps(obj)
-    
-    print("export var json_str = '{}' ".format(json_obj) )
-
-
 results = connect("corner")
 print(len(results))
 CreateJson(results, "corners.js")
